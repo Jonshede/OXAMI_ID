01be3e0ce24b7b1fe7ee97da81f862f918dd064b
Johan Jonshede <jjonshede@outlook.com>
2026-05-04 12:08:09 +0200

    First structure

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OXAMI_ID.DataLayer
+{
+    internal class Class1
+    {
+        // Placeholder
+    }
+}
---
+++ LogicLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OXAMI_ID.LogicLayer
+{
+    internal class Class1
+    {
+        // Placeholder
+    }
+}
